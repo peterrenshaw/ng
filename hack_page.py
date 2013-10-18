90f7c8b976cd3b02400ea0a0ec849b85a2cea077
Peter Renshaw <goonmail@netspace.net.au>
2013-10-18 22:22:27 +1100

    2013OCT18 * basic hack working - hack_page.py now working for 1 instance & no index

--- hack_page.py
+++ hack_page.py
@@ -3,10 +3,12 @@
 
 
 import os
+import string
+from string import Template
 
 
 class Page:
-    def __init__(self):
+    def __init__(self, is_index):
         """initialise the Page"""
         # basic page
         self.__header = ""     # template
@@ -22,16 +24,17 @@ class Page:
         self.file_data = dict(path="",     # valid, relative fp to basepath
                               name="",
                               ext="",
-                              fpn="")      # path/filename.ext as url !filesys
+                              filepathname="")      # path/filename.ext as url !filesys
         # meta data
-        self.meta_data = dict(tags=[],
+        self.meta_data = dict(author="",
+                              tags=[],
                               date="",
                               year="",
                               month="",
                               mmm="",
                               mm="",
                               day="",
-                              is_index=False)
+                              is_index=(True if is_index else False))
                          
     # --- collect data ---
     #
@@ -43,17 +46,15 @@ class Page:
         return False
     def body(self, title, abstract, content):
         """body of page"""
+        status = False
         if title:
-            #self.title = title
-            self.body_data['title'] = title
             if abstract:
-                #self.abstract = abstract
-                self.body_data['abstract'] = abstract
-                if content: 
-                    #self.content = content # optional content
+                if content:
+                    self.body_data['title'] = title
+                    self.body_data['abstract'] = abstract
                     self.body_data['content'] = content
-                    return True # all ok, else fail
-        return False
+                    status = True # all ok, else fail
+        return status
     def footer(self, content):
         """html/text template for footer"""
         if content:
@@ -69,8 +70,8 @@ class Page:
                     self.file_data['path'] = path
                     self.file_data['ext'] = ext
                     self.file_data['name'] = "%s.%s" % (name, ext)
-                    self.file_data['fpn'] = "%s/%s" % (self.file_data['path'], 
-                                                       self.file_data['name'])
+                    self.file_data['filepathname'] = "%s/%s" % (self.file_data['path'], 
+                                                                self.file_data['name'])
                     status = True
         return status
     def metadata(self, **kwargs):
@@ -111,24 +112,101 @@ class Page:
         return self.get('meta', key)
     def get_file(self, key):
         """return file_data by key or F"""
-        return self.get('fire', key)
+        return self.get('file', key)
     def get_body(self, key):
         """return body_data by key or F"""
         return self.get('body', key)
     # --- end call data
-    # --- build ---
-    def build(self):
+    def build_template(self, template, data):
+        """given template and data, return substituted str"""
+        # TODO:  no testing here
+        data_rendered = []
+        data_raw = template.split("\n")
+        for line in data_raw:
+            t = string.Template(line)
+            render = str(t.substitute(data))
+            data_rendered.append(render)
+        return  data_rendered
+    # --- render ---
+    def render(self):
         """"build a page from bits of data"""
         # index page OR content page
-        
-    # --- show ---
-    def render(self):
-        """render page, save as file"""
-        pass 
+        if not self.get_meta('is_index'):
+            # normal content page
+            #
+
+            # build header
+            header_map = dict(author=self.get_meta('author'),
+                              title=self.get_body('title'),
+                              abstract=self.get_body('abstract'), 
+                              year=self.get_meta('year'),
+                              mmm=self.get_meta('mmm'),
+                              day=self.get_meta('day'))
+            header = self.build_template(self.__header, header_map)
+            
+            # build content
+            # assuming no templating in content
+            content = self.get_body('content')
+
+            # build footer
+            # assuming no templating in footer
+            footer = "%s" % self.__footer
+
+            try:
+                print("writing to <%s>" % self.get_file('filepathname'))
+                with open(self.get_file('filepathname'),'wt') as f:
+                    for line in header:
+                        f.write("%s\n" % line)
+                    if content:
+                        f.write(content)
+                    if footer:
+                        f.write(footer)
+                    print("written")
+                    f.close()
+            except:
+                data = ""
+                if f: f.close()
+                return False
+            else:
+                return True
+        else:
+            # index page
+            pass
+    # --- render ---
 
 
 def main():
-    pass
+    destination = 'E:\\blog\\seldomlogical'
+
+    h = ""
+    hp = os.path.join(os.curdir, 'source', 'partials', 'header.html')
+    with open(hp, encoding='utf-8') as f:
+        h = f.read()
+    
+
+    ft = ""
+    fp = os.path.join(os.curdir, 'source', 'partials', 'footer.html')
+    with open(fp, encoding='utf-8') as f:
+        ft = f.read()
+   
+    c = ""
+    cp = os.path.join(os.curdir, 'source', 'page.txt')
+    with open(cp, encoding='utf-8') as f:
+        c = f.read()
+
+    t = "Hello world"
+    a = "A quick hello world hack. Not much too look at but you have to start somewhere."
+    is_index=False
+
+    p = Page(is_index)
+    p.header(h)
+    p.footer(ft)
+    if p.body(title=t, abstract=a, content=c):
+        if p.filename(path=destination, name="PAGE", ext="html"):
+            if p.metadata(tags=['tag1','tag2'],author="peterrenshaw",
+                          year="2013",mm="10",mmm="OCT",day="17"):
+                if p.render():
+                    print("ok")
 
 #---
 # main app entry point
