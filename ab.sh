7160816d42962644ea9634d7cfab8f9e70ebf470
Peter Renshaw <goonmail@netspace.net.au>
2013-10-30 13:23:54 +1100

    2013OCT30 * crazy checkin - lots of work, bits on the floor, some parts don't fit - lots of hammering

---
+++ ab.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# ~*~ encoding: utf-8 ~*~
+
+#---
+# name: ab.sh
+# date: 2013OCT27
+# prog: pr
+# desc: sick of typing in stuff, reduce to:
+#           - sh ab.sh win32
+#           - ab.sh *inux
+# refs: <http://tldp.org/HOWTO/Bash-Prog-Intro-HOWTO-2.html>
+#---
+
+# kill old install
+echo '--- autobuild ---'
+echo 'rm -fr *crap*'
+rm -fr build distro ng.egg-info
+
+# rebuild
+echo 'python setup install'
+python setup.py install
+
+echo '--- ack ---'
+
+# vim: ff=unix:ts=4:sw=4:tw=78:noai:expandtab
